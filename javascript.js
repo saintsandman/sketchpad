0f3de785e177d96e8f1367f874517029ce57aee1
saintsandman <thesaintofsand@gmail.com>
2024-04-14 21:57:38 -0400

    Set grid stylings

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+
+
+/* 16*16 grid of square divs
+make divs with javascript
+put them inside a container div
+use flexbox to make them appear as a grid
+
+ok what do i do. 
+basically
+how to make a box in a div */
