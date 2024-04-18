48e21b429d2d47c797f075e9e825dfdbbd6b3062
saintsandman <thesaintofsand@gmail.com>
2024-04-18 16:39:09 -0400

    Add row of 16 boxes

--- javascript.js
+++ javascript.js
@@ -9,4 +9,20 @@ use flexbox to make them appear as a grid
 
 ok what do i do. 
 basically
-how to make a box in a div */
+how to make a box in a div
+ok did that. now i need to make divs 16* 16. 
+so just like 16 rows with 16 boxes in them each
+make like a loop
+
+*/
+
+//selects grid container
+
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+
+  box = document.createElement("div");
+  container.appendChild(box);
+
+}
