507490dc0b976e2054218686aeb9ffa1bd9019dc
saintsandman <thesaintofsand@gmail.com>
2024-04-19 12:39:34 -0400

    Add class to square divs

--- javascript.js
+++ javascript.js
@@ -3,26 +3,18 @@
 
 
 /* 16*16 grid of square divs
-make divs with javascript
-put them inside a container div
-use flexbox to make them appear as a grid
-
-ok what do i do. 
-basically
-how to make a box in a div
-ok did that. now i need to make divs 16* 16. 
-so just like 16 rows with 16 boxes in them each
-make like a loop
+when you hover mouse over a div/boxes, 
+it changes the color
 
 */
 
-//selects grid container
+
 
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
-
   box = document.createElement("div");
+  box.classList.add("box");
   container.appendChild(box);
-
 }
+
