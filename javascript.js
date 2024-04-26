2506882bd8841c46103011502628592495c69148
saintsandman <thesaintofsand@gmail.com>
2024-04-25 22:55:00 -0400

    Add number of boxes and rows to grid based on user number

--- javascript.js
+++ javascript.js
@@ -1,32 +1,41 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-  box = document.createElement("div");
-  box.classList.add("box");
-  container.appendChild(box);
-}
-
-let boxes = document.querySelectorAll("div.box");
-
-boxes.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "red";
-  });
-});
-
-function removeGrid() {
-  boxes.forEach((square) => {
-    square.remove();
-  })
-}
-
 function changeSize(size) {
   for (let i = 0; i < size; i++) {
-    row = document.createElement("div");
+    let row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
   }
+  
+  let rows = document.querySelectorAll("div.row");
+
+  rows.forEach((row) => {
+    for (let i = 0; i < size; i++) {
+      box = document.createElement("div");
+      box.classList.add("box");
+      row.appendChild(box);
+    }
+  
+  let boxes = document.querySelectorAll("div.box");
+
+  boxes.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "red";
+    });
+  });
+  })
 }
+
+changeSize(16);
+
+function removeGrid() {
+  let rows = document.querySelectorAll("div.row");
+  rows.forEach((row) => {
+    row.remove();
+  })
+}
+
+
 
 btn = document.querySelector("button");
 
@@ -41,5 +50,8 @@ use size as input
 function that deletes all of the divs
 then makes a # of containers = to the input number
 then it creates and appends a number of square divs = to input number into each container
-start with that */ 
+start with that
+
+ok now you need to do the row thing to the original 
+16x16 so its the same sort of row with boxes in em system.*/ 
 
